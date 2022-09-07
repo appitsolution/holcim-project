e74aa32c97cc9b9dcc6fd84c22371e07c77cf288
appitionitsolution <applicationitsolution@gmail.com>
2022-09-07 10:33:07 +0300

    check

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,46 +4,135 @@ import "./home.css";
 import logo from "../images/Logo_Holcim.png";
 import contentImg from "../images/content-image.jpg";
 const Home = () => {
+  const [intp1, setInpt1] = useState({
+    name: "Revenue Up",
+    value: 1,
+  });
+  const [intp2, setInpt2] = useState({
+    name: "Cost Down",
+    value: 1,
+  });
+  const [intp3, setInpt3] = useState({
+    name: "Safety",
+    value: 1,
+  });
+  const [intp4, setInpt4] = useState({
+    name: "Sustainability",
+    value: 1,
+  });
+  const [intp5, setInpt5] = useState({
+    name: "Customer Experience",
+    value: 1,
+  });
+
+  const [intp6, setInpt6] = useState({
+    name: "Employee Experience",
+    value: 1,
+  });
+  const [intp7, setInpt7] = useState({
+    name: "New Customer Groups",
+    value: 1,
+  });
   const [state, setState] = useState([
-    {
-      name: "Revenue Up",
-      value: 1,
-    },
-    {
-      name: "Cost Down",
-      value: 1,
-    },
-    {
-      name: "Safety",
-      value: 1,
-    },
-    {
-      name: "Sustainability",
-      value: 1,
-    },
-    {
-      name: "Customer Experience",
-      value: 1,
-    },
-    {
-      name: "Employee Experience",
-      value: 1,
-    },
-    {
-      name: "New Customer Groups",
-      value: 1,
-    },
+    intp1,
+    intp2,
+    intp3,
+    intp4,
+    intp5,
+    intp6,
+    intp7,
   ]);
   const navigate = useNavigate();
 
   const readInput = ({ target }) => {
-    const result = state.map((num) => {
-      if (num.name !== target.name) {
-        return num;
-      }
-      return { name: num.name, value: target.value };
-    });
-    setState(result);
+    switch (target.name) {
+      case intp1.name:
+        setInpt1({ name: intp1.name, value: target.value });
+        setState([
+          { name: intp1.name, value: target.value },
+          intp2,
+          intp3,
+          intp4,
+          intp5,
+          intp6,
+          intp7,
+        ]);
+        break;
+      case intp2.name:
+        setInpt2({ name: intp2.name, value: target.value });
+        setState([
+          intp1,
+          { name: intp2.name, value: target.value },
+          intp3,
+          intp4,
+          intp5,
+          intp6,
+          intp7,
+        ]);
+        break;
+      case intp3.name:
+        setInpt3({ name: intp3.name, value: target.value });
+        setState([
+          intp1,
+          intp2,
+          { name: intp3.name, value: target.value },
+          intp4,
+          intp5,
+          intp6,
+          intp7,
+        ]);
+        break;
+      case intp4.name:
+        setInpt4({ name: intp4.name, value: target.value });
+        setState([
+          intp1,
+          intp2,
+          intp3,
+          { name: intp4.name, value: target.value },
+          intp5,
+          intp6,
+          intp7,
+        ]);
+        break;
+      case intp5.name:
+        setInpt5({ name: intp5.name, value: target.value });
+        setState([
+          intp1,
+          intp2,
+          intp3,
+          intp4,
+          { name: intp5.name, value: target.value },
+          intp6,
+          intp7,
+        ]);
+        break;
+      case intp6.name:
+        setInpt6({ name: intp6.name, value: target.value });
+        setState([
+          intp1,
+          intp2,
+          intp3,
+          intp4,
+          intp5,
+          { name: intp6.name, value: target.value },
+          intp7,
+        ]);
+        break;
+      case intp7.name:
+        setInpt7({ name: intp7.name, value: target.value });
+        setState([
+          intp1,
+          intp2,
+          intp3,
+          intp4,
+          intp5,
+          intp6,
+          { name: intp7.name, value: target.value },
+        ]);
+        break;
+      default:
+        break;
+    }
   };
   const totalResult = () => {
     const mass = [
@@ -144,28 +233,146 @@ const Home = () => {
               </h1>
               <form className="content-logic-form">
                 <ul className="content-logic-list">
-                  {state.map((num, index) => (
-                    <li className="content-logic-item" key={index}>
-                      <p className="content-logic-text">{num.name}</p>
-                      <input
-                        className="content-logic-input"
-                        type="range"
-                        value={num.value}
-                        min="1"
-                        max="5"
-                        step="1"
-                        name={num.name}
-                        onInput={readInput}
-                      />
-                      <ul className="content-logic-input-list">
-                        <li className="content-logic-input-item">1</li>
-                        <li className="content-logic-input-item">2</li>
-                        <li className="content-logic-input-item">3</li>
-                        <li className="content-logic-input-item">4</li>
-                        <li className="content-logic-input-item">5</li>
-                      </ul>
-                    </li>
-                  ))}
+                  <li className="content-logic-item">
+                    <p className="content-logic-text">{intp1.name}</p>
+                    <input
+                      className="content-logic-input"
+                      type="range"
+                      value={intp1.value}
+                      min="1"
+                      max="5"
+                      step="1"
+                      name={intp1.name}
+                      onInput={readInput}
+                    />
+                    <ul className="content-logic-input-list">
+                      <li className="content-logic-input-item">1</li>
+                      <li className="content-logic-input-item">2</li>
+                      <li className="content-logic-input-item">3</li>
+                      <li className="content-logic-input-item">4</li>
+                      <li className="content-logic-input-item">5</li>
+                    </ul>
+                  </li>
+                  <li className="content-logic-item">
+                    <p className="content-logic-text">{intp5.name}</p>
+                    <input
+                      className="content-logic-input"
+                      type="range"
+                      value={intp5.value}
+                      min="1"
+                      max="5"
+                      step="1"
+                      name={intp5.name}
+                      onInput={readInput}
+                    />
+                    <ul className="content-logic-input-list">
+                      <li className="content-logic-input-item">1</li>
+                      <li className="content-logic-input-item">2</li>
+                      <li className="content-logic-input-item">3</li>
+                      <li className="content-logic-input-item">4</li>
+                      <li className="content-logic-input-item">5</li>
+                    </ul>
+                  </li>
+                  <li className="content-logic-item">
+                    <p className="content-logic-text">{intp2.name}</p>
+                    <input
+                      className="content-logic-input"
+                      type="range"
+                      value={intp2.value}
+                      min="1"
+                      max="5"
+                      step="1"
+                      name={intp2.name}
+                      onInput={readInput}
+                    />
+                    <ul className="content-logic-input-list">
+                      <li className="content-logic-input-item">1</li>
+                      <li className="content-logic-input-item">2</li>
+                      <li className="content-logic-input-item">3</li>
+                      <li className="content-logic-input-item">4</li>
+                      <li className="content-logic-input-item">5</li>
+                    </ul>
+                  </li>
+                  <li className="content-logic-item">
+                    <p className="content-logic-text">{intp6.name}</p>
+                    <input
+                      className="content-logic-input"
+                      type="range"
+                      value={intp6.value}
+                      min="1"
+                      max="5"
+                      step="1"
+                      name={intp6.name}
+                      onInput={readInput}
+                    />
+                    <ul className="content-logic-input-list">
+                      <li className="content-logic-input-item">1</li>
+                      <li className="content-logic-input-item">2</li>
+                      <li className="content-logic-input-item">3</li>
+                      <li className="content-logic-input-item">4</li>
+                      <li className="content-logic-input-item">5</li>
+                    </ul>
+                  </li>
+                  <li className="content-logic-item">
+                    <p className="content-logic-text">{intp3.name}</p>
+                    <input
+                      className="content-logic-input"
+                      type="range"
+                      value={intp3.value}
+                      min="1"
+                      max="5"
+                      step="1"
+                      name={intp3.name}
+                      onInput={readInput}
+                    />
+                    <ul className="content-logic-input-list">
+                      <li className="content-logic-input-item">1</li>
+                      <li className="content-logic-input-item">2</li>
+                      <li className="content-logic-input-item">3</li>
+                      <li className="content-logic-input-item">4</li>
+                      <li className="content-logic-input-item">5</li>
+                    </ul>
+                  </li>
+                  <li className="content-logic-item">
+                    <p className="content-logic-text">{intp7.name}</p>
+                    <input
+                      className="content-logic-input"
+                      type="range"
+                      value={intp7.value}
+                      min="1"
+                      max="5"
+                      step="1"
+                      name={intp7.name}
+                      onInput={readInput}
+                    />
+                    <ul className="content-logic-input-list">
+                      <li className="content-logic-input-item">1</li>
+                      <li className="content-logic-input-item">2</li>
+                      <li className="content-logic-input-item">3</li>
+                      <li className="content-logic-input-item">4</li>
+                      <li className="content-logic-input-item">5</li>
+                    </ul>
+                  </li>
+                  <li className="content-logic-item">
+                    <p className="content-logic-text">{intp4.name}</p>
+                    <input
+                      className="content-logic-input"
+                      type="range"
+                      value={intp4.value}
+                      min="1"
+                      max="5"
+                      step="1"
+                      name={intp4.name}
+                      onInput={readInput}
+                    />
+                    <ul className="content-logic-input-list">
+                      <li className="content-logic-input-item">1</li>
+                      <li className="content-logic-input-item">2</li>
+                      <li className="content-logic-input-item">3</li>
+                      <li className="content-logic-input-item">4</li>
+                      <li className="content-logic-input-item">5</li>
+                    </ul>
+                  </li>
                 </ul>
                 <button className="content-logic-button" onClick={totalResult}>
                   MaQe it happen
